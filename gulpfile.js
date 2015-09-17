cecf028f319a7c8c2d5f94a632bce87252679109
Pierre-Yves Poujade <py@poujade.org>
2015-09-17 10:10:56 -0400

    Add lint task

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var gutil = require('gulp-util');
+var jshint = require('gulp-jshint');
 
-gulp.task('mocha', function() {
+gulp.task('mocha', ['lint'], function() {
   return gulp.src(['test/**/*.js'], {read: false})
     .pipe(mocha({ reporter: 'list' }))
 });
 
-gulp.task('watch-mocha', function() {
-  gulp.watch(['lib/**', 'helpers/**', 'test/**', 'index.js'], ['mocha']);
+gulp.task('dev', function() {
+  gulp.watch(['lib/**', 'helpers/**', 'test/**', 'index.js'], ['mocha'], {read: false});
+});
+
+gulp.task('lint', function() {
+  return gulp.src(['lib/**/*.js', 'helpers/**/*.js', 'test/**/*.js', 'index.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
 });
