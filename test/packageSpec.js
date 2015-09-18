093fa17869890285e24efc109ac6d507fada31dd
Pierre-Yves Poujade <py@poujade.org>
2015-09-18 09:25:59 -0400

    Add hash generator

--- test/packageSpec.js
+++ test/packageSpec.js
@@ -1,44 +1,90 @@
 var expect = require('chai').expect;
 var Package = require('../lib/package');
+var sinon = require('sinon');
 
 describe('Package', function() {
-  it('should create new package when passed a valid config', function() {
-    var config = {
-      recipient: 'Example',
-      files: {
-        fileA: '/path/to/filea.ext',
-        fileB: 'relative/path/to/fileb.ext'
-      }
-    };
-    var subject = new Package(config);
+  describe('#constructor', function() {
+    it('should create new package when passed a valid config', function() {
+      var config = {
+        recipient: 'Example',
+        files: {
+          fileA: 'test/fixtures/fileA.test',
+          fileB: 'test/fixtures/fileB.test'
+        }
+      };
+      var subject = new Package(config);
 
-    expect(subject).to.deep.equal(config);
+      expect(subject).to.have.property('recipient', config.recipient);
+      expect(subject).to.have.property('files', config.files);
+    });
+
+    it('should throw with no config', function() {
+      var subject = function() { new Package(); };
+
+      expect(subject).to.throw(TypeError, /invalid or empty/i);
+    });
+
+    it('should throw without an object as config', function() {
+      var config = 'invalid';
+      var subject = function() { new Package(config); };
+      
+      expect(subject).to.throw(TypeError, /invalid or empty/i);
+    });
+
+    it('should throw with an incomplete config', function() {
+      var config = { recipient: 'Test' };
+      var subject = function() { new Package(config); };
+      
+      expect(subject).to.throw(Error, /missing/i);
+    });
+
+    it('should throw with a bad config.files', function() {
+      var config = { recipient: 'Test', files: 'invalid' };
+      var subject = function() { new Package(config); };
+
+      expect(subject).to.throw(TypeError, /not a string/i);
+    });
   });
 
-  it('should throw with no config', function() {
-    var subject = function() { new Package(); };
+  describe('#get_name', function() {
+    var config;
+    var subject;
 
-    expect(subject).to.throw(TypeError, /invalid or empty/i);
-  });
+    beforeEach(function() {
+      config = {
+        recipient: 'Test',
+        files: {
+          fileA: 'test/fixtures/fileA.test',
+          fileB: 'test/fixtures/fileB.test'
+        }
+      };
 
-  it('should throw without an object as config', function() {
-    var config = 'invalid';
-    var subject = function() { new Package(config); };
-    
-    expect(subject).to.throw(TypeError, /invalid or empty/i);
-  });
+      subject = new Package(config);
+    });
 
-  it('should throw with an incomplete config', function() {
-    var config = { recipient: 'Test' };
-    var subject = function() { new Package(config); };
-    
-    expect(subject).to.throw(Error, /missing/i);
-  });
+    it('should return a string', function(done) {
+      subject.get_name(function(err, name) {
+        if (err) return done(err);
+        expect(typeof(name)).to.eq('string');
+        return done();
+      });
+    });
 
-  it('should throw with a bad config.files', function() {
-    var config = { recipient: 'Test', files: 'invalid' };
-    var subject = function() { new Package(config); };
+    it("should include the recipient's lowercase name", function(done) {
+      subject.get_name(function(err, name) {
+        if (err) return done(err);
+        expect(name).to.match(/test/);
+        return done();
+      });
+    });
 
-    expect(subject).to.throw(TypeError, /not a string/i);
+    it('should include a 6 chars hash', function(done) {
+      var hash_regex = /[a-f0-9]{6}/;
+      subject.get_name(function(err, name) {
+        if (err) return done(err);
+        expect(name).to.match(hash_regex);
+        return done();
+      });
+    });
   });
 });
