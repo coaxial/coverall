302340baa437c89925501ff6d270c19f1c1a832a
Pierre-Yves Poujade <py@poujade.org>
2015-09-20 09:37:19 -0400

    Add istanbul test coverage

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,50 @@ var mocha = require('gulp-mocha');
 var gutil = require('gulp-util');
 var jshint = require('gulp-jshint');
 var console = require('better-console');
+var runSequence = require('run-sequence');
+var istanbul = require('gulp-istanbul');
 
-gulp.task('mocha', ['lint'], function() {
-  return gulp.src(['test/**/*_spec.js'], {read: false})
-    .pipe(mocha({ reporter: 'list' }));
+var spec_files = ['test/**/*_spec.js'];
+var every_js_file = ['lib/**/*.js', 'helpers/**/*.js', 'test/**/*.js', 'index.js'];
+var coverage_report_dir = 'test/coverage';
+var mocha_reporter = 'list';
+
+gulp.task('mocha', function() {
+  return gulp.src(spec_files, { read: false })
+    .pipe(mocha({ reporter: mocha_reporter }));
+});
+
+gulp.task('mocha-istanbul', function(cb) {
+  gulp.src(every_js_file)
+    .pipe(istanbul({ includeUntested: true }))
+    .pipe(istanbul.hookRequire())
+    .on('finish', function () {
+      gulp.src(spec_files)
+        .pipe(mocha({ reporter: mocha_reporter }))
+        .pipe(istanbul.writeReports( { reporters: ['html', 'text'] }))
+        .on('end', cb);
+    });
 });
 
 gulp.task('dev', function() {
-  gulp.watch(['lib/**', 'helpers/**', 'test/**', 'index.js'], ['mocha'], {read: false});
+  gulp.watch(every_js_file, ['test'], { read: false });
 });
 
-gulp.task('lint', ['clear-console'], function() {
-  return gulp.src(['lib/**/*.js', 'helpers/**/*.js', 'test/**/*.js', 'index.js'])
+gulp.task('lint', function(cb) {
+  return gulp.src(every_js_file)
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
+    cb(err);
 });
 
 gulp.task('clear-console', function() {
   return console.clear();
 });
+
+gulp.task('test', function(cb) {
+  runSequence('clear-console',
+              'lint',
+              'mocha-istanbul',
+              cb);
+});
+
